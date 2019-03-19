3366421036da2652a8a786567f03905d3aaf6ad3
szincone <sawyerzincone@gmail.com>
2019-03-19 11:42:19 -0600

    finished classes

--- src/classes.py
+++ src/classes.py
@@ -22,9 +22,11 @@ class LatLon:
 
 class Waypoint (LatLon):
     def __init__(self, name, lat, lon):
+        super().__init__(lat, lon)
         self.name = name
-        LatLon.lat = lat
-        LatLon.lon = lon
+
+    def __str__(self):
+        return (f"Waypoint {self.name} is at {self.lat} {self.lon}")
 
 
 wp_obj = Waypoint('space', 20, 20)
@@ -35,18 +37,41 @@ print(wp_obj.name, wp_obj.lat, wp_obj.lon)
 
 # YOUR CODE HERE
 
+
+class Geocache (Waypoint):
+    def __init__(self, name, difficulty, size, lat, lon):
+        super().__init__(name, lat, lon)
+        self.difficulty = difficulty
+        self.size = size
+
+    def __str__(self):
+        result_string = (
+            f"Geocache {self.name} is at {self.lat} {self.lon} "
+            f"and is rated {self.difficulty}.")
+        return result_string
+
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 
-# YOUR CODE HERE
 
+# YOUR CODE HERE
+waypoint = Waypoint('Catacombs', 41.70505, -121.51521)
+# print(waypoint.name, waypoint.lat, waypoint.lon)
+geo_cata = Geocache('Catacombs', 'superhard',
+                    '4 sq. miles', 41.70505, -121.51521)
+# print(geo_cata.name, geo_cata.difficulty,
+#       geo_cata.size, geo_cata.lat, geo_cata.lon)
 
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
-# print(waypoint)
+print(waypoint)
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
 
 # YOUR CODE HERE
+geocache = Geocache('Newberry Views', 'diff 1.5',
+                    'size 2', 44.052137, -121.41556)
+# print(geo_berry.name, geo_berry.difficulty,
+#       geo_berry.size, geo_berry.lat, geo_berry.lon)
 
 # Print it--also make this print more nicely
-# print(geocache)
+print(geocache)
